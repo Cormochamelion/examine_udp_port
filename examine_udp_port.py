0cd6c11137c5d06d80c87540a51d6666622ab76b
Cormochamelion <70096814+Cormochamelion@users.noreply.github.com[D>
2024-06-12 19:28:26 +0200

    examine_udp_port: Add send functionality

--- examine_udp_port.py
+++ examine_udp_port.py
@@ -1,5 +1,22 @@
+import dotenv
+import nclib
+import os
 from itertools import product
 
+dotenv.load_dotenv()
+
+host = os.environ["EXAM_HOST"]
+port = os.environ["EXAM_PORT"]
+
 combs = map(
-    lambda tup: b"".join([bytes(x) for x in tup]), product(range(128), repeat=2)
+    lambda tup: bytes("".join([chr(x) for x in tup]), "utf8"),
+    product(range(128), repeat=2),
 )
+
+nc = nclib.Netcat(connect=(host, int(port)), echo_hex=True, udp=True, verbose=True)
+
+for comb in combs:
+    pad_char = b"\x00"
+    payload = (pad_char * 2) + comb + (pad_char * 7) + b"\x0a"
+    nc.send(payload)
+    # reply = nc.recv(16, timeout=2)
